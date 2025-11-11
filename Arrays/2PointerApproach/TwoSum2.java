35bd3f5c90a7d1f23ea8c136677d5661d5345131
Yash Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2025-11-11 17:07:24 +0530

    Added Two Sum II

---
+++ Arrays/2PointerApproach/TwoSum2.java
@@ -0,0 +1,32 @@
+/**
+ * Problem: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/     
+ * Difficulty: Medium
+ * Approach: 2 Pointer
+ * Time Complexity: O()
+ * Space Complexity: O()
+ * 
+ * Author: Yash Yadav
+ * Date: 11
+ */
+
+class Solution {
+ public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int sum = 0;
+        while(left < right){
+            sum = numbers[left] + numbers[right];
+
+            if(sum > target){
+                right--;
+            }
+            else if(sum < target){
+                left++;
+            }
+            else if(sum == target){
+                return new int[]{left + 1, right + 1};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
