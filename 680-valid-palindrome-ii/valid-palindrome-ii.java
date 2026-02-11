a505de0ad6f1200a63ebe3dcf99b301ec14ba6ca
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-11 17:37:21 +0530

    Time: 4 ms (99.48%) | Memory: 47.9 MB (45.31%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,9 +1,8 @@
 class Solution {
-    public boolean helper(int a, int b, String s){
+    public boolean helper(int a, int b , String s){
         while( a < b ){
-            if(s.charAt(a) != s.charAt(b)){
-                return false;
-            }
+            char left = s.charAt(a), right = s.charAt(b);
+            if(left != right) return false;
             else{
                 a++;
                 b--;
@@ -12,20 +11,18 @@ class Solution {
         return true;
     }
     public boolean validPalindrome(String s) {
-        int a = 0,
-            b = s.length() - 1;
-
-        while( a < b ){
-            char left = s.charAt(a),
-                right = s.charAt(b);
-            if(left != right){
-                return helper(a+1, b , s) || helper(a, b - 1, s);
-            }
-            else{
-                a++;
-                b--;
-            }
-        }
-        return true;
+      int a = 0, b = s.length() - 1;
+      while(a < b){
+         char left = s.charAt(a), right = s.charAt(b);
+         if(left != right){
+            // return the function call
+            return helper(a + 1, b, s) || helper(a , b - 1, s);
+         }
+         else{
+            a++;
+            b--;
+         }
+      }
+      return true;
     }
 }
