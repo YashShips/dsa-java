cf9d33501d166778f3d2373e8719bb3ff3f41895
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-12 17:35:34 +0530

    Time: 3 ms (49.74%) | Memory: 43.7 MB (98.92%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,8 +1,11 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
+        /**
+        1. Sort 
+        2. converging pointer checking between a range, takes all the values in between as the pointer refers to largest sum, so all the sums in between will automatically be a pair that meets the condition.
+         */
         Collections.sort(nums);
-        int a = 0, b = nums.size() - 1;
-        int count = 0;
+        int a = 0, b = nums.size() - 1, count = 0;
         while( a < b ){
             int sum = nums.get(a) + nums.get(b);
             if( sum < target ){
