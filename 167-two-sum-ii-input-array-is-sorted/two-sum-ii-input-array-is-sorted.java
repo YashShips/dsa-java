c6db65ae423c4b5f4900701a6ba6c1c0d74510af
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-12 18:22:28 +0530

    Time: 1 ms (99.82%) | Memory: 48.4 MB (78.93%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -5,12 +5,10 @@ class Solution {
         int sum = numbers[a] + numbers[b];
         if(sum == target) return new int[]{a + 1, b+1};
         else if(sum > target){
-            b--;
-            sum = 0;
+            b--;   
         }
         else{
             a++;
-            sum = 0;
         }
        }
        return new int[]{0,0};
