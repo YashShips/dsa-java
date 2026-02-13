8b25d6ef7f8e0aab10499faeb35dcc8731233972
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-13 18:35:53 +0530

    Time: 26 ms (99.19%) | Memory: 59.2 MB (59.09%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,28 @@
 class Solution {
     public void helper(int f, int[] nums, List<List<Integer>> res){
-        int i = f + 1, j = nums.length - 1;
-        while( i < j ){
-            int sum = nums[f] + nums[i] + nums[j];
-            if(sum > 0){
-                j--;
-            }
-            else if(sum < 0){
-                i++;
-            }
+        int a = f + 1, b = nums.length - 1;
+        while( a < b ){
+            int sum = nums[f] + nums[a] + nums[b];
+            if(sum > 0) b--;
+            else if(sum < 0) a++;
             else{
-                res.add(Arrays.asList(nums[f],nums[i],nums[j]));
-                i++;
-                j--;
-                while(i<j && nums[i] == nums[i - 1]) i++;
-                while(i<j && nums[j] == nums[j + 1]) j--;
+                res.add(Arrays.asList(nums[f], nums[a], nums[b]));
+                a++;
+                b--;
+                while(a < b && nums[a] == nums[a - 1]) a++;
+                while(a < b && nums[b] == nums[b + 1]) b--;
             }
         }
     }
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0) break;
-            else if(i == 0 || nums[i] != nums[i - 1]){
-                helper(i, nums, res);
-            }
+       Arrays.sort(nums);
+       List<List<Integer>> res = new ArrayList<>();
+       for(int i = 0; i < nums.length; i++){
+        if(nums[i] > 0) break;
+        else if(i == 0 || nums[i] != nums[i - 1]){
+            helper(i, nums, res);
         }
-        return res;
+       }
+       return res;
     }
 }
