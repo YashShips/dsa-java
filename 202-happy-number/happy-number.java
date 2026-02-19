12b5d0923947fc4972e4acf02f7f60cbeebaa859
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-19 18:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (32.79%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -2,23 +2,22 @@ class Solution {
     public boolean isHappy(int n) {
        int slow = n,
         fast = n;
-        
-        while(fast != 1){
-            slow = getNextNumber(slow);
-            fast = getNextNumber(getNextNumber(fast));
 
+        while(fast != 1){
+            slow = getNum(slow);
+            fast = getNum(getNum(fast));
             if(fast == 1) return true;
-            if(slow == fast) return false;
+            if(fast == slow) return false;
         }
         return true;
     }
-    public int getNextNumber(int n ){
-        int output = 0;
-        while(n > 0){
+    public int getNum(int n){
+        int sum = 0;
+        while( n > 0 ){
             int digit = n % 10;
-            output += digit * digit;
+            sum += digit * digit;
             n = n / 10;
         }
-        return output;
+        return sum;
     }
 }
