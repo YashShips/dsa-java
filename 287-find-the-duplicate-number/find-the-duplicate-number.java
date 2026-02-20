fa110401b23b1290e357fe390bc33651a0434050
Yash Yadav <yash.yadav.cse@gmail.com>
2026-02-20 21:08:57 +0530

    Time: 4 ms (91.24%) | Memory: 82.8 MB (69.30%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -4,17 +4,19 @@ class Solution {
             fast = 0;
         do{
             slow = nums[slow];
+            // fast = nums[fast];
+            // fast = nums[fast];  isko agar shortcut m likhna h toh hum likhenge
             fast = nums[nums[fast]];
         }
         while(slow != fast);
 
         int n1 = 0,
             n2 = slow;
-        
+
         while(n1 != n2){
             n1 = nums[n1];
             n2 = nums[n2];
         }
-        return n1;
+        return n2;
     }
 }
