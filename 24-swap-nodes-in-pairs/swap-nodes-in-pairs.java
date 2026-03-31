e33882e2440013f1a6d9a95a78a621cf0ea7afca
Yash Yadav <yash.yadav.cse@gmail.com>
2026-03-31 21:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (38.49%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -9,21 +9,24 @@
  * }
  */
 class Solution {
+    public void realSwap(ListNode prev, ListNode curr){
+        if(curr == null || curr.next == null) return;
+        ListNode f = curr,
+                 s = curr.next;
+
+                 f.next = s.next;
+                 s.next = f;
+                 prev.next = s;
+
+                 realSwap(f, f.next);
+
+    }
     public ListNode swapPairs(ListNode head) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode p = dummy;
-        while(head != null && head.next != null){
-            ListNode f = head,
-                     s = head.next;
+        ListNode prev = dummy;
 
-                     f.next = s.next;
-                     s.next = f;
-                     p.next = s;
-
-                     p = f;
-                     head = f.next;
-        }
+        realSwap(prev, head);
         return dummy.next;
     }
 }
