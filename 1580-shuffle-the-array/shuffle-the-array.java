472217602808836c6731657407773a9d12bffb3e
Yash Yadav <yash.yadav.cse@gmail.com>
2026-04-13 20:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (92.92%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[2*n];
+        int a = 0,
+            b = n;
+        for(int i = 0; i < 2*n; i += 2){
+            arr[i] = nums[a];
+            arr[i + 1] = nums[b];
+            a++;
+            b++;
+        }
+        return arr;
+    }
+}
