bd892b1d02f3dd851abd49f178bff06c064e42b2
Yash Yadav <yash.yadav.cse@gmail.com>
2026-05-26 18:00:16 +0530

    Time: 2 ms (5.36%) | Memory: 44.7 MB (7.45%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -6,7 +6,7 @@ class Solution {
         return res;
     }
     public void cb(int idx, int[] nums, List<List<Integer>> res, List<Integer> ds){
-        res.add(new ArrayList<>(ds));
+            res.add(new ArrayList<>(ds));
         for(int i = idx; i < nums.length; i++){
             ds.add(nums[i]);
             cb(i + 1, nums, res, ds);
