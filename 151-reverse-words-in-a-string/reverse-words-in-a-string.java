2b91a19a9edff766e40162f71096577e88dfccdc
Yash Yadav <yash.yadav.cse@gmail.com>
2026-07-14 17:29:58 +0530

    Time: 8 ms (36.16%) | Memory: 44.7 MB (28.75%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,49 +1,15 @@
 class Solution {
     public String reverseWords(String s) {
-        int l = 0, r = s.length() - 1;
-
-        // Trim left
-        while (l <= r && s.charAt(l) == ' ') l++;
-
-        // Trim right
-        while (r >= l && s.charAt(r) == ' ') r--;
-
+        Stack<String> stack = new Stack<>();
+        String[] ans = s.trim().split("\\s+");
         StringBuilder sb = new StringBuilder();
-
-        // Remove extra spaces
-        while (l <= r) {
-            if (s.charAt(l) != ' ') {
-                sb.append(s.charAt(l));
-            } else {
-                if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
-                    sb.append(' ');
-                }
-            }
-            l++;
+        for(String str : ans){
+            stack.push(str);
         }
-
-        // Reverse entire string
-        reverse(sb, 0, sb.length() - 1);
-
-        // Reverse each word
-        int start = 0;
-        for (int end = 0; end <= sb.length(); end++) {
-            if (end == sb.length() || sb.charAt(end) == ' ') {
-                reverse(sb, start, end - 1);
-                start = end + 1;
-            }
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+            sb.append(" ");
         }
-
-        return sb.toString();
-    }
-
-    private void reverse(StringBuilder sb, int i, int j) {
-        while (i < j) {
-            char temp = sb.charAt(i);
-            sb.setCharAt(i, sb.charAt(j));
-            sb.setCharAt(j, temp);
-            i++;
-            j--;
-        }
+        return sb.toString().trim();
     }
 }
