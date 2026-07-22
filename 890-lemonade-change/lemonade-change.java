776a105a119d53036b1fd8b173fe70ee0c5f825d
Yash Yadav <yash.yadav.cse@gmail.com>
2026-07-22 17:38:49 +0530

    Time: 2 ms (98.71%) | Memory: 72.8 MB (13.30%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for(int num : bills){
+            if(num == 5) five++;
+            else if(num == 10){
+                if(five == 0) return false;
+                else{
+                    five--;
+                    ten++;
+                }       
+            }
+            else if(num == 20){
+                if(five > 0 && ten > 0){
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3){
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
